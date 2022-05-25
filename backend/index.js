ca46ce43ba91c427dfb9dc4da53b4f2ed73e70bd
JhonatanD09 <joranada@gmail.com>
2022-05-24 19:40:22 -0500

    swagger-doc-categoryes-post

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 import { setUp } from './connection'
+const path = require('path')
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
 
 setUp()
 
@@ -9,11 +13,30 @@ var app = express()
 
 var port = process.env.S_PORT
 
+
+const swaggerSpec =  {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info:{
+            title: 'Entre nubes bar app API',
+            version: '1.0.0',
+            
+        },
+        servers:[{
+            url: `http://localhost:8000` 
+         }]
+    },
+    apis: [`${path.join(__dirname,'./routes/*.js')}`],
+}
+
+
 // middlewares
 app.use(morgan(':method :url - :referrer :status :response-time ms'));
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cors())
+
+app.use('/api-doc',swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerSpec)))
 
 // test
 app.get('/msg', (req, res) => res.json({ msg: "API works"}))
--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,7 +1,40 @@
 const router = require("express").Router();
 const controller = require('../controllers/category.controller')
 
+
+/**
+ * @swagger
+ *components:
+ *  schemas:
+ *      Category:
+ *          type: object
+ *          properties:
+ *              name : 
+ *                  type: string
+ *          example: 
+ *              name : Bebidas
+ *              
+ */
+
+
 router.get('/', controller.getAll);
+
+/**
+ * @swagger
+ * /category:
+ *  post:
+ *      sumary : create a new category
+ *      tags : [Category]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              aplication/json:
+ *                  schema:
+ *                      type : object
+ *                      $ref: '#/components/schemas/Category'
+ *      response:
+ *          200:
+ */
 router.post('/', controller.createCategory);
 router.put('/:id', controller.editCategory)
 router.delete('/:id', controller.deleteCategory)
