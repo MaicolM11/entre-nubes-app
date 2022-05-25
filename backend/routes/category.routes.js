dfffb4c1d52d59e3ef8f14d4691f99654a7608e9
JhonatanD09 <joranada@gmail.com>
2022-05-24 20:01:33 -0500

    category-doc-api-complete

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -16,7 +16,22 @@ const controller = require('../controllers/category.controller')
  *              
  */
 
-
+/**
+ * @swagger
+ * /category:
+ *  get:
+ *      sumary : get all categories
+ *      tags : [Category]   
+ *      responses:
+ *          200:
+ *              description : all categories!!
+ *              content:
+ *                  aplication/json:
+ *                      schema:
+ *                          type : array
+ *                          items:
+ *                              $ref: '#/components/schemas/Category'
+ */
 router.get('/', controller.getAll);
 
 /**
@@ -37,7 +52,56 @@ router.get('/', controller.getAll);
  *              description : new categoty created!!
  */
 router.post('/', controller.createCategory);
+
+/**
+ * @swagger
+ * /category/{id}:
+ *  put:
+ *      sumary : update category
+ *      tags : [Category]
+ *      parameters:
+ *          - in : path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description : category id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              aplication/json:
+ *                  schema:
+ *                      type : object
+ *                      $ref: '#/components/schemas/Category'
+ *      responses:
+ *          200:
+ *              description : category update sucesfull
+ *          404:
+ *              description:  category not found
+ *                  
+ */
 router.put('/:id', controller.editCategory)
+
+/**
+ * @swagger
+ * /category/{id}:
+ *  delete:
+ *      sumary : delete category
+ *      tags : [Category]
+ *      parameters:
+ *          - in : path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description : category id
+ *      responses:
+ *          200:
+ *              description : category eliminated
+ *          404:
+ *              description:  category not found
+ *                  
+ */
 router.delete('/:id', controller.deleteCategory)
 
 module.exports = router;
