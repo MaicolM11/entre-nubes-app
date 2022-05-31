8bc5612dc75f3e1371184a43121194fb1917ed1b
JhonatanD09 <joranada@gmail.com>
2022-05-30 19:16:04 -0500

    modal-message

---
+++ src/components/ModalMessage/ModalMessage.js
@@ -0,0 +1,22 @@
+import React  from "react";
+import Button from "../Button/Button";
+import './ModalMessage.css'
+
+const ModalMessage = ({text, setOpenModal})=>{
+    
+    const closeModal = ()=> setOpenModal(false)
+
+    return(
+        <div className="modalContainer">
+                <div className="title">
+                    <h1>{text}</h1>
+                </div>
+            <div className="footer">
+            <Button text="Aceptar" color="#2CE74A" width= {185} 
+            height = {45} handleOnClick = {closeModal}/>
+            </div>
+        </div> 
+    )
+}
+
+export default ModalMessage
--- src/pages/componentTest/Test.js
+++ src/pages/componentTest/Test.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CardProduct from '../../components/Cards/CardProduct/CardProduct';
+import ModalMessage from '../../components/ModalMessage/ModalMessage';
 
 const test = () => {
 
+  const [modalOpen, setModalOpen] = useState(false);
+
     const url = 'https://toctocdelivery.co/wp-content/uploads/2019/04/aguila-cerveza.png'
     const des = "Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure"
+
+
+    const click = () => setModalOpen ((o)=> !o)
 
   return (
     <div >
@@ -13,6 +19,11 @@ const test = () => {
       category = "Cerveza"
       description={des} units={10} 
       unitsSale={7}/>
+
+      <button style={{backgroundColor: '#4674F2'}} onClick={click}> Mostrar modal </button>
+
+      {modalOpen && <ModalMessage text="Producto agregado correctamente!" setOpenModal={setModalOpen}/>}
+      
     </div>
   );
 };
