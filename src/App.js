a8dac868e633bf295042f3a8a9094d3e1394011a
JhonatanD09 <joranada@gmail.com>
2022-06-01 17:13:06 -0500

    prodcu-stock-modal-update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login/Login';
 import Homepage from "./pages/homepage/Homepage";
+import Test from './pages/tests/Test';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/homepage/*" element={<Homepage />} />
       </Routes>
     </BrowserRouter>
