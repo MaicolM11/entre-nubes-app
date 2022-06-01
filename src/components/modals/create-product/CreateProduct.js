df4e5fef3dbd87994a58f177fc453d8645f1060e
jerobytes <jerobytes@gmail.com>
2022-06-01 18:27:48 -0500

    Refactored create product modal

--- src/components/modals/create-product/CreateProduct.js
+++ src/components/modals/create-product/CreateProduct.js
@@ -1,46 +1,67 @@
 import React from "react";
+import './CreateProduct.css';
+import '../../../styles/ModalStyle.css';
 
 import { Button } from '../../buttons/button/Button'
-import TextInput from '../../inputs/TextInput'
-import Select from '../../select/Select'
 
-import '../styles/ModalStyles.css'
-import './CreateProduct.css'
+import TextInput from '../../inputs/TextInput';
+import Select from '../../select/Select';
+
+import Close from '../../../assets/icons/close.svg';
+import Wine from '../../../assets/icons/wine-bottle.svg';
+import Money from '../../../assets/icons/money.svg';
+import Circle from '../../../assets/icons/circle.svg';
 
 const CreateProduct = ({ title, nameButton }) => {
     return (
         <div className="modal-container">
-            <div className="add-producto-container">
-                <h1 className="add-title">{title}</h1>
-                <div className="inputs">
-                    <img className="image-product"></img>
-                    <div className="inputs-container">
-                        <TextInput
-                            type='medium' name='add-product'
-                            placeholder='Nombre del producto' onChange />
-                        <Select
-                            size="normal"
+            <div className="create-product-container">
+                <div className="modal-title-container">
+                    <h1 className="add-title-modal">{title}</h1>
+                    <div className="close-image-container">
+                        <img
+                            src={Close}
+                            alt="icon"
+                            className="close-img"
                         />
-                        <TextInput
-                            type='medium' name='price-product'
-                            placeholder='Precio por unidad' onChange />
-                        <TextInput
-                            type='medium' name='units-product'
-                            placeholder='Unidades de venta' onChange />
-                        <h2>Descripción del producto </h2>
-                        <textarea></textarea>
-
                     </div>
                 </div>
-                <div className="button-create">
-                <Button
-                    theme="ok" size="normal">
-                    {nameButton}
-                </Button>
+                <div className="modal-center-container">
+                    <div className="components-modal">
+                        <div className="img-modal-area">
+                            <div className="product-image-modal"></div>
+                        </div>
+                        <div className="data-modal-area">
+                            <TextInput
+                                type='text'
+                                name='add-product'
+                                icon={Wine}
+                                placeholder='Nombre del producto' />
+                            <Select size="normal" />
+                            <TextInput
+                                type='text'
+                                name='price-product'
+                                icon={Money}
+                                placeholder='Precio por unidad' />
+                            <TextInput
+                                type='text'
+                                name='units-product'
+                                icon={Circle}
+                                placeholder='Unidades de venta' />
+                            <div className="text-area-container">
+                                <label className="description-title">Descripción del Producto</label>
+                                <textarea className="description-area" />
+                            </div>
+                            <Button
+                                theme="ok" size="normal">
+                                {nameButton}
+                            </Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CreateProduct
+export default CreateProduct;
