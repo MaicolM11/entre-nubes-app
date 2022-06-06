76451d3f26e615a752c18831da9ba3dbc05d58b1
jerobytes <jerobytes@gmail.com>
2022-06-06 15:03:49 -0500

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import "./App.css"
+import React from "react";
+import "./App.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Login from './pages/login/Login';
+import Login from "./pages/login/Login";
 import AdminHomepage from "./pages/admin-homepage/AdminHomepage";
-import SalesmanHomepage from './pages/salesman-homepage/SalesmanHomepage';
+import SalesmanHomepage from "./pages/salesman-homepage/SalesmanHomepage";
 
-import { getMyInfo } from './services/user';
+import { getMyInfo } from "./services/user";
 
 const SalesmanProtectedRoute = ({ children }) => {
   let user = true;
@@ -34,17 +34,21 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />} />
-        <Route path="/admin/*" element = {
+        <Route
+          path="/admin/*"
+          element={
             <AdminProtectedRoute>
               <AdminHomepage />
             </AdminProtectedRoute>
-          } 
+          }
         />
-        <Route path="/salesman/*" element= { 
+        <Route
+          path="/salesman/*"
+          element={
             <SalesmanProtectedRoute>
               <SalesmanHomepage />
             </SalesmanProtectedRoute>
-          } 
+          }
         />
       </Routes>
     </BrowserRouter>
--- src/components/inputs/PasswordInput.js
+++ src/components/inputs/PasswordInput.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./InputStyle.css";
 
-import { Input } from './InputStyle';
+import { InputStyle } from "./InputStyle";
 
-import Lock from '../../assets/icons/lock.svg';
+import Lock from "../../assets/icons/lock.svg";
 
 const PasswordInput = ({ placeholder, onChange }) => {
   return (
-    <div className='input'>
-      <img
-        src={Lock}
-        alt="icon"
-        className="img"
-      />
-      <Input
+    <div className="input">
+      <img src={Lock} alt="icon" className="input-icon" />
+      <InputStyle
         size="normal"
         type="password"
         name="password"
         placeholder={placeholder}
-        onChange={onChange} />
+        onChange={onChange}
+      />
     </div>
   );
 };
