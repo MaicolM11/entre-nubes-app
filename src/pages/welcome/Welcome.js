48d470431f906efb390969f5b42863a5722b83d9
jerobytes <jerobytes@gmail.com>
2022-07-19 10:10:50 -0500

    Welcome messages in progress

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -1,7 +1,7 @@
 import React from "react";
+import "./Welcome.css";
 import styled from "styled-components";
 import { colors } from "../../components/styles/colors";
-import "./Welcome.css";
 
 const WelcomeContainer = styled.div`
   display: flex;
@@ -14,15 +14,74 @@ const WelcomeContainer = styled.div`
 
 const WelcomeCenterContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
   padding: 50px 100px;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
 `;
 
-const Welcome = () => {
+const WelcomeImgContainer = styled.div`
+  display: flex;
+  width: 375px;
+  height: 375px;
+  background-color: darkgoldenrod;
+`;
+
+const WelcomeTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 590px;
+  height: 119px;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  background-color: darkkhaki;
+`;
+
+const WelcomeBoldTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 432px;
+  height: 61px;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  background-color: darkcyan;
+`;
+
+const WelcomeSystemTitleLabel = styled.label`
+  color: ${colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  font-family: var(--roboto);
+  white-space: nowrap;
+`;
+
+const WelcomeSystemTextLabel = styled.label`
+  text-align: center;
+  line-height: 21px;
+  color: ${colors.text};
+  font-size: 16px;
+  font-weight: 500;
+  font-family: var(--roboto);
+`;
+
+const Welcome = ({ img, title, subTitle, description }) => {
   return (
     <div className="welcome-container">
       <WelcomeContainer>
-        <WelcomeCenterContainer></WelcomeCenterContainer>
+        <WelcomeCenterContainer>
+          <WelcomeImgContainer>{img}</WelcomeImgContainer>
+          <WelcomeTextContainer>
+            <WelcomeBoldTextContainer>
+              <WelcomeSystemTitleLabel>{title}</WelcomeSystemTitleLabel>
+              <WelcomeSystemTitleLabel>{subTitle}</WelcomeSystemTitleLabel>
+            </WelcomeBoldTextContainer>
+            <WelcomeSystemTextLabel>{description}</WelcomeSystemTextLabel>
+          </WelcomeTextContainer>
+        </WelcomeCenterContainer>
       </WelcomeContainer>
     </div>
   );
