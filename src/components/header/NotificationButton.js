41c16b9c76f032c38b91482813650ad225efd2f0
jerobytes <jerobytes@gmail.com>
2022-07-30 17:29:44 -0500

    Notifications dropdown in progress

--- src/components/header/NotificationButton.js
+++ src/components/header/NotificationButton.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Notification } from "../../assets/icons/notification.svg";
 import { colors } from "../styles/colors";
+
+const NotificationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30px;
+`;
 
 const NotificationButtonContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 30px;
   transition: 0.2s;
   cursor: pointer;
   user-select: none;
@@ -23,15 +28,45 @@ const NotificationButtonContainer = styled.div`
   }
 `;
 
+const NotificationDropdownContainer = styled.div`
+  display: flex;
+  width: 190px;
+  position: absolute;
+  top: 65px;
+  transform: translateX(-85%);
+  overflow: hidden;
+  background-color: ${colors.secondary};
+  border: solid 1px ${colors.border};
+  border-radius: 16px;
+`;
+
+const NotificationDropdownCenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 20px;
+`;
+
 const NotificationButton = () => {
+  const [isOpenNotifications, setIsOpenNotifications] = useState(false);
+
   const showNotifications = () => {
-    console.log("Show notifications...");
+    setIsOpenNotifications(!isOpenNotifications);
   };
 
   return (
-    <NotificationButtonContainer onClick={showNotifications}>
-      <Notification />
-    </NotificationButtonContainer>
+    <NotificationContainer>
+      <NotificationButtonContainer onClick={showNotifications}>
+        <Notification />
+      </NotificationButtonContainer>
+      {isOpenNotifications && (
+        <NotificationDropdownContainer>
+          <NotificationDropdownCenterContainer>
+            <label>item 1</label> <label>item 2</label>
+          </NotificationDropdownCenterContainer>
+        </NotificationDropdownContainer>
+      )}
+    </NotificationContainer>
   );
 };
 
