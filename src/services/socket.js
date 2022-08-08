6c5c70266c02f9fcef80dd6829923cc568695df8
MaicolM11 <mmoarias4@gmail.com>
2022-08-08 11:46:46 -0500

    ws

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 import { getToken } from "./storage";
 
-export const getSocket = () => io("ws://localhost:8000", { query: { token: getToken() } });
+export const getSocket = () => io("https://entre-nubes.herokuapp.com/", { query: { token: getToken() } });
